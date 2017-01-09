94cef07fff053e53501b86f212cb5494714bf85c
Peter Csajtai <peter.csajtai@logmein.com>
2017-01-09 16:44:53 +0100

    Added xml documentation

--- src/stashbox.web.mvc/StashboxDependencyResolver.cs
+++ src/stashbox.web.mvc/StashboxDependencyResolver.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using Ronin.Common;
 using Stashbox.Infrastructure;
 
 namespace Stashbox.Web.Mvc
 {
+    /// <summary>
+    /// Represents the stashbox dependency resolver.
+    /// </summary>
     public class StashboxDependencyResolver : System.Web.Mvc.IDependencyResolver
     {
         private readonly IStashboxContainer stashboxContainer;
 
+        /// <summary>
+        /// Constructs a <see cref="StashboxDependencyResolver"/>
+        /// </summary>
+        /// <param name="stashboxContainer"></param>
         public StashboxDependencyResolver(IStashboxContainer stashboxContainer)
         {
+            Shield.EnsureNotNull(stashboxContainer, nameof(stashboxContainer));
+
             this.stashboxContainer = stashboxContainer;
         }
-
+        
+        /// <inheritdoc />
         public object GetService(Type serviceType)
         {
             return this.stashboxContainer.Resolve(serviceType);
         }
 
+        /// <inheritdoc />
         public IEnumerable<object> GetServices(Type serviceType)
         {
             return this.stashboxContainer.ResolveAll(serviceType);
