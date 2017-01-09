49fb7d3f34126a2e2ec8f514d7253ba4401f3874
Peter Csajtai <peter.csajtai@logmein.com>
2017-01-09 16:10:56 +0100

    Initial commit

---
+++ src/stashbox.web.mvc/StashboxDependencyResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Stashbox.Infrastructure;
+
+namespace Stashbox.Web.Mvc
+{
+    public class StashboxDependencyResolver : System.Web.Mvc.IDependencyResolver
+    {
+        private readonly IStashboxContainer stashboxContainer;
+
+        public StashboxDependencyResolver(IStashboxContainer stashboxContainer)
+        {
+            this.stashboxContainer = stashboxContainer;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this.stashboxContainer.Resolve(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.stashboxContainer.ResolveAll(serviceType);
+        }
+    }
+}
