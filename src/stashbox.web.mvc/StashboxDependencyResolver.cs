6192f9077c70a67297e9790da7268c498cf41a0a
Peter Csajtai <peter.csajtai@logmein.com>
2017-02-07 14:16:41 +0100

    Updated stashbox added preapplication http module

--- src/stashbox.web.mvc/StashboxDependencyResolver.cs
+++ src/stashbox.web.mvc/StashboxDependencyResolver.cs
@@ -2,6 +2,7 @@
 using Stashbox.Utils;
 using System;
 using System.Collections.Generic;
+using System.Web;
 
 namespace Stashbox.Web.Mvc
 {
@@ -10,6 +11,7 @@ namespace Stashbox.Web.Mvc
     /// </summary>
     public class StashboxDependencyResolver : System.Web.Mvc.IDependencyResolver
     {
+        private const string ScopeKey = "requestScope";
         private readonly IStashboxContainer stashboxContainer;
 
         /// <summary>
@@ -26,13 +28,32 @@ namespace Stashbox.Web.Mvc
         /// <inheritdoc />
         public object GetService(Type serviceType)
         {
-            return this.stashboxContainer.CanResolve(serviceType) ? this.stashboxContainer.Resolve(serviceType) : null;
+            return this.GetScope().CanResolve(serviceType) ? this.GetScope().Resolve(serviceType) : null;
         }
 
         /// <inheritdoc />
         public IEnumerable<object> GetServices(Type serviceType)
         {
-            return this.stashboxContainer.CanResolve(serviceType) ? this.stashboxContainer.ResolveAll(serviceType) : new List<object>();
+            return this.GetScope().CanResolve(serviceType) ? this.GetScope().ResolveAll(serviceType) : new List<object>();
+        }
+
+        /// <summary>
+        /// Closes the current per request scope.
+        /// </summary>
+        public static void TerminateScope()
+        {
+            var scope = HttpContext.Current.Items[ScopeKey] as IStashboxContainer;
+            scope?.Dispose();
+        }
+
+        private IStashboxContainer GetScope()
+        {
+                var scope = HttpContext.Current.Items[ScopeKey] as IStashboxContainer;
+
+                if (scope == null)
+                    HttpContext.Current.Items[ScopeKey] = scope = this.stashboxContainer.BeginScope();
+
+                return scope;
         }
     }
 }
